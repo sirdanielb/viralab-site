244e023f008d3ca2b0d6a6e634a01d0cf2b26f0b
sirdanielb <daninbox22@gmail.com>
2026-02-05 16:47:53 -0500

    Create HeroSection.tsx

---
+++ src/sections/HeroSection.tsx
@@ -0,0 +1,8 @@
+export default function HeroSection() {
+  return (
+    <section id="hero" style={{ minHeight: '100vh' }}>
+      <h1>VIRALAB</h1>
+      <p>Built to spread.</p>
+    </section>
+  );
+}
